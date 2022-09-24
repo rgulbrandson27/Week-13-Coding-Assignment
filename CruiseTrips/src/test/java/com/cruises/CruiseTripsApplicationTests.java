054ffd3b315e9a6282255323aa2dd9a2a01ab139
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2022-09-23 19:13:50 -0500

    First commit

---
+++ CruiseTrips/src/test/java/com/cruises/CruiseTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiseTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
