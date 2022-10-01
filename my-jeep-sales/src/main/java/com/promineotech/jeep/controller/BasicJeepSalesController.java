12c291e0d1b082e92da684f68cfe43f7fa983f4e
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2022-10-01 01:20:09 -0500

    first commit

--- my-jeep-sales/src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ my-jeep-sales/src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -4,17 +4,27 @@ import java.util.List;
 
 import javax.swing.Spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
+import com.promineotech.jeep.service.JeepSalesService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j 
+
 public class BasicJeepSalesController implements JeepSalesController {
 	
-
-public List<Jeep> fetchJeeps(String mdoel, String trim) {
-	return null;
+@Autowired
+private JeepSalesService jeepSalesService;
+	
+@Override
+public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+	log.info("model={}, trim-{}", model, trim);
+	return jeepSalesService.fetchJeeps(model, trim);
 }
 
 
